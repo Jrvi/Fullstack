e8db5e7fceb9c506a84a93c91ffcc11f4d467015
Järvi Juho Kalle Kristian <juho.kk.jarvi@student.jyu.fi>
2021-07-27 19:27:13 +0300

    1.1 tehty

--- osa1/kurssitiedot/appi/src/App.js
+++ osa1/kurssitiedot/appi/src/App.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.Total}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,19 +35,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total Total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
+
 }
 
 export default App
