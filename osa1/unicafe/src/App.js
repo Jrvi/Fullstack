4c8079e9c38bb0b65500fcb39630b671989881a9
Juho <juho.kk.jarvi@student.jyu.fi>
2021-08-31 14:21:58 +0300

    1.7 tehty

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,60 +1,76 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
+  /* tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
-  const [helper, setHelper] = useState(0)
+  const [bad, setBad] = useState(0) */
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    setHelper(helper + 1)
-    updateStatistics()
-  }
+  const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0})
+  const [statictics, setStatictics] = useState({all: 0, average: 0, positive: 0})
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    updateStatistics()
-  }
+const Display = () => {
+  return (
+    <div>
+      <p>
+        good {feedback.good} <br/>
+        neutral {feedback.neutral} <br/>
+        bad {feedback.bad} <br/>
+        all {statictics.all} <br/>
+        average {statictics.average} <br/>
+        positive {statictics.positive} % <br/>
+        </p>
+    </div>
+  )
+}
 
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    setHelper(helper - 1)
-    updateStatistics()
+const handleStatictics = (props) => {
+  const newStatictics = {
+    all: props.good + props.neutral + props.bad,
+    average: (props.good - props.bad) / (props.good + props.neutral + props.bad),
+    positive: props.good / (props.good + props.neutral + props.bad)
   }
+  setStatictics(newStatictics)
+}
 
-  const updateStatistics = () => {
-    const newAverage = (helper / all)
-    const newPositive = (good / all)
-    setAll(all + 1)
-    setAverage(newAverage)
-    setPositive(newPositive * 100)
+const handleGood = () => {
+  const newFeedback = {
+    good: feedback.good + 1,
+    neutral: feedback.neutral,
+    bad: feedback.bad
   }
+  setFeedback(newFeedback)
+  handleStatictics(newFeedback)
+}
+
+const handleNeutral = () => {
+  const newFeedback = {
+    good: feedback.good,
+    neutral: feedback.neutral + 1,
+    bad: feedback.bad
+  }
+  setFeedback(newFeedback)
+  handleStatictics(newFeedback)
+}
+
+const handleBad = () => {
+  const newFeedback = {
+    good: feedback.good ,
+    neutral: feedback.neutral,
+    bad: feedback.bad + 1
+  }
+  setFeedback(newFeedback)
+  handleStatictics(newFeedback)
+}
 
   return (
     <div>
       <h1>give feedback</h1>
-      <div>
-        <button onClick={handleGoodClick}>good</button>
-        <button onClick={handleNeutralClick}>neutral</button>
-        <button onClick={handleBadClick}>bad</button>
-      </div>
-      <div>
-        <h1>statistics</h1>
-      </div>
-      <div>
-        <p>good {good}<br></br>
-        neutral {neutral}<br></br>
-        bad {bad}<br></br>
-        all {all}<br></br>
-        average {average}<br></br>
-        positive {positive} %
-        </p>
-      </div>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h1>statictics</h1>
+      <Display/>
     </div>
   )
 }
